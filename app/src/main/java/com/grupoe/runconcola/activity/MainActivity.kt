6263bb7c50443dab24dc75489a9b790d9c4c9b4c
Emanuel <emanuel.1kll@gmail.com>
2022-12-01 08:30:05 -0400

    Commit with view binding 1/12/2022

---
+++ app/src/main/java/com/grupoe/runconcola/activity/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.grupoe.runconcola.activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grupoe.runconcola.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
