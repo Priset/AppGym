8abea290a2a1de62b4d40e338b860c22102c29f9
Jhosias Laguna <jhosiasmauricio@gmail.com>
2022-12-09 07:24:20 -0400

    Commit 09/12 start recycle leg

--- app/src/main/java/com/grupoe/runconcola/activity/AvailableOptionsActivity.kt
+++ app/src/main/java/com/grupoe/runconcola/activity/AvailableOptionsActivity.kt
@@ -1,12 +1,19 @@
 package com.grupoe.runconcola.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.grupoe.runconcola.R
 
-class activity_AvailableOptions : AppCompatActivity() {
+class AvailableOptionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_available_options)
+    }
+
+    fun Leg(view: View) {
+        val legview = Intent(this,SpecificExerciseActivity::class.java)
+        startActivity(legview)
     }
 }
