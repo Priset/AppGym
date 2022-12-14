99f298d50fde59b485a069939d56b0c86ed2f60d
Jhosias Laguna <jhosiasmauricio@gmail.com>
2022-12-13 20:49:06 -0400

    Commit 13/12 fixed recycle view without click, change background recycle and xml standardized

--- app/src/main/java/com/grupoe/runconcola/adapter/AvailableOptionsAdapter.kt
+++ app/src/main/java/com/grupoe/runconcola/adapter/AvailableOptionsAdapter.kt
@@ -1,32 +1,47 @@
 package com.grupoe.runconcola.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.grupoe.runconcola.activity.OptionsRecycleActivity
+import com.grupoe.runconcola.activity.armsActivity.ExerciseArmsActivity
+import com.grupoe.runconcola.activity.backActivity.ExerciseBackActivity
+import com.grupoe.runconcola.activity.legsActivity.ExerciseLegsActivity
 import com.grupoe.runconcola.dataClasses.Muscle
-import com.grupoe.runconcola.databinding.ActivityAvailableOptionsBinding
+import com.grupoe.runconcola.databinding.ItemAvailableOptionsBinding
 
 class AvailableOptionsAdapter: RecyclerView.Adapter<AvailableOptionsAdapter.AvailableOptionsActivityViewHolder>() {
-    private val availableOptions = mutableListOf<Muscle>()//
+    private val availableOptions = mutableListOf<Muscle>()
+    private var context: Context? = null
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):AvailableOptionsActivityViewHolder =
-        AvailableOptionsActivityViewHolder(
-            ActivityAvailableOptionsBinding.inflate(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):AvailableOptionsActivityViewHolder {
+        context = parent.context
+        return AvailableOptionsActivityViewHolder(
+            ItemAvailableOptionsBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
             )
         )
+    }
     override fun onBindViewHolder(holder: AvailableOptionsActivityViewHolder, position: Int){
         holder.binding(availableOptions[position])
     }
     override fun getItemCount(): Int = availableOptions.size
 
-    inner class AvailableOptionsActivityViewHolder(private val binding: ActivityAvailableOptionsBinding):
+    inner class AvailableOptionsActivityViewHolder(private val binding: ItemAvailableOptionsBinding):
     RecyclerView.ViewHolder(binding.root){
         fun binding(data: Muscle){
             binding.exerciseName.text = data.type
-            binding.legDay.setImageResource(data.image)
+            binding.itemOption.setImageResource(data.image)
+            binding.card1.setOnClickListener{
+                val intent = Intent(context,ExerciseLegsActivity::class.java)
+                //intent.putExtra("change", ExerciseBackActivity.)
+                context?.startActivity(intent)
+            }
 
         }
     }
--- app/src/main/java/com/grupoe/runconcola/dataClasses/Muscle.kt
+++ app/src/main/java/com/grupoe/runconcola/dataClasses/Muscle.kt
@@ -2,7 +2,7 @@ package com.grupoe.runconcola.dataClasses
 
 import android.graphics.drawable.Drawable
 
-class Muscle (
+data class Muscle (
     var type: String,
     var image: Int
 
